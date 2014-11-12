74aa2815cf560b2db2c373ebab54d17d6122cf69
unknown <arafats@DHA11ARAFATS3.ap.corp.yr.com>
2014-11-12 12:18:07 +0600

    New projects created for POC

---
+++ CustomerManagement/CustomerManagement.Web/Controllers/CustomerController.cs
@@ -0,0 +1,137 @@
+﻿using CustomerManagement.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerManagement.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private static CustomerCollection _Customers;
+
+        static CustomerController()
+        {
+            _Customers = new CustomerCollection();
+            Customer cust = new Customer();
+            cust.Id = 1;
+            cust.FirstName = "Shibli";
+            cust.MiddleName = "Mohammad";
+            cust.LastName = "Arafat";
+            cust.Address = "Dhammondi, Dhaka";
+            cust.MobileNo = "0171";
+            _Customers.Add(cust);
+            cust = new Customer();
+            cust.Id = 2;
+            cust.FirstName = "Erfana";
+            cust.MiddleName = "";
+            cust.LastName = "Sikder";
+            cust.Address = "Gulshan, Dhaka";
+            cust.MobileNo = "0181";
+            _Customers.Add(cust);
+            cust = new Customer();
+            cust.Id = 3;
+            cust.FirstName = "Iffat";
+            cust.MiddleName = "";
+            cust.LastName = "Jabin";
+            cust.Address = "Khilkhet, Dhaka";
+            cust.MobileNo = "0161";
+            _Customers.Add(cust);
+        }
+
+        public ActionResult Index()
+        {            
+            return View(_Customers);
+        }
+
+        //
+        // GET: /Customer/Details/5
+
+        public ActionResult Details(int id)
+        {
+            ViewBag.Message = string.Format("Details of Customer: {0}", id);
+            return View(_Customers.Find(x=> x.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Details(Customer customer)
+        {
+            //ViewBag.Message = string.Format("Details of Customer: {0}", id);
+            _Customers.Update(customer);
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Customer/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Customer/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Customer/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Message = string.Format("Details of Customer: {0}", id);
+            return View(_Customers.Find(x => x.Id == id));
+        }
+
+        //
+        // POST: /Customer/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Customer customer)
+        {
+            //ViewBag.Message = string.Format("Details of Customer: {0}", id);
+            _Customers.Update(customer);
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Customer/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Customer/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CustomerManagement/CustomerManagement.Web/Models/CustomerModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerManagement.Web.Models
+{
+    public class Customer
+    {
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Mobile No")]
+        public string MobileNo { get; set; }
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+    }
+
+    public class CustomerCollection : List<Customer>
+    {
+        internal void Update(Customer customer)
+        {
+            this.RemoveAll(x => x.Id == customer.Id);
+            this.Add(customer);
+        }
+    }
+}
